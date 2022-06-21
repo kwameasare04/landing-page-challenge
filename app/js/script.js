76b6eddd24f894a59c7e68959474d880b41c38a5
louis <louisasare@hotmail.com>
2022-06-21 01:37:57 +0100

    added animation to mobile hamburger

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,20 @@
-console.log("hello world");
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+btnHamburger.addEventListener('click', function(){
+console.log("click hamburger");
+
+if(header.classList.contains('open')){
+    header.classList.remove('open'); ///close hamburger menu
+    overlay.classList.remove('fade-in');
+    overlay.classList.add('fade-out');
+    
+}
+else {
+header.classList.add('open'); ///open hamburger menu
+overlay.classList.remove('fade-out');
+overlay.classList.add('fade-in');
+}
+
+});
